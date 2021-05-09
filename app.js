aa4a20f22f951f5a304879f7d54848b46c641db4
MEA-23 <77838294+MEA-23@users.noreply.github.com>
2021-05-09 22:24:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,12 @@
       const num = document.querySelector('#content');
       const clearButton = document.querySelector('#t0');
       
-      
+      const witH =document.querySelector('#with');
      
-      clearButton.onclick = function (){
-        clear();
-      };
+      const darkButton = document.querySelector('#black');
+  
+  
+      
  
       function nightMood (){
       body.style.backgroundColor = 'black';
@@ -21,10 +22,7 @@
       }
       
       
-      const darkButton = document.querySelector('#black');
-      darkButton.onclick = function (){
-        nightMood();
-      };
+      
   
       
   
@@ -49,11 +47,14 @@
 
       }
   
-      const lightButton = document.querySelector('#white');
+  
+  
+ 
+  
       
-      lightButton.onclick = function (){
-        lightMood();
-      };
+      
+      
+  
       
       function lightMood (){
       body.style.backgroundColor = 'white';
@@ -61,22 +62,55 @@
       }
   
   
-      const witH =document.querySelector('#with');
-  
-    witH.onclick =function (){
-      showButton();
-    } ;
  
+  
+ 
+ 
+     function hideButton() {
+       hiddenButton.style.cssText = "height :1300px; display: flex; align-items:center ;justify-content:center ; font-size: 40pt;color:#1a703f;";}
+
+
+
      function showButton() {
        hiddenButton.style.cssText = 
    ' height :800px; margin: 300px 100px  50px 100px; display: flex;align-items:center ;justify-content:center ;font-size: 40pt;color:#1a703f;border-radius: 50%; background-color:#72eeb386;';
      }
+
+
+       clearButton.onclick = function (){
+        clear();
+      };
+       
+       
   
-   const without =document.querySelector('#without');
-  
-    without.onclick =function (){
-      hideButton();
+    witH.onclick =function (){
+      if(witH.value==="false"){
+        
+      showButton();
+      witH.value="true";
+      witH.textContent="اخفاء الزر";
+      }
+      else if(witH.value==="true"){
+        hideButton();
+     witH.value="false";
+      witH.textContent="إظهار الزر";
+      }
     } ;
  
-     function hideButton() {
-       hiddenButton.style.cssText = "height :1300px; display: flex; align-items:center ;justify-content:center ; font-size: 40pt;color:#1a703f;";}
+ 
+ 
+    darkButton.onclick = function (){
+        if(darkButton.value==='false'){
+          nightMood();
+          darkButton.value='true';
+          darkButton.textContent="الوضع الفاتح";
+          darkButton.style.backgroundColor='#e7e7e7';
+        }
+        else if (darkButton.value ==='true')
+        {
+          lightMood ();
+          darkButton.value= 'false';
+          darkButton.textContent="الوضع الداكن";
+          darkButton.style.backgroundColor='#555555';
+        }
+      };
