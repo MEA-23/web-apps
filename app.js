53f7d417c4b8fb7f7e2a4de8732ab214e4d7fdfb
MEA-23 <77838294+MEA-23@users.noreply.github.com>
2021-05-14 20:39:36 +0200

    update app.js

--- app.js
+++ app.js
@@ -23,26 +23,26 @@
       
       let time;
   const reSize = () =>{
-    if (hiddenButton.style.height ==='800px' ||  hiddenButton.style.height ==='750px'){
+    if (hiddenButton.style.height ==='700px' ||  hiddenButton.style.height ==='600px'){
    clearTimeout(time);
-   hiddenButton.style.height ='750px';
-   hiddenButton.style.width ='750px';
+   hiddenButton.style.height ='600px';
+   hiddenButton.style.width ='600px';
    
  // time = setTimeout(function() {
 
-  hiddenButton.style.height ='800px';
-   hiddenButton.style.width ='800px';
+  hiddenButton.style.height ='700px';
+   hiddenButton.style.width ='700px';
 //}, 400);
 } };
       
-      hiddenButton.innerHTML ='<img height="800px" width="800px" src="pic2.png" style="border-radius: 50%;">';
+      hiddenButton.innerHTML ='<img height="700px" width="700px" src="pic2.png" style="border-radius: 50%;">';
       
  let img = document.querySelector('img');
       function clear() {
         num.textContent ='0';
         text= 0;
-        img.style.height = '800px';
-        img.style.width = '800px';
+        img.style.height = '700px';
+        img.style.width = '700px';
       }
       
       
